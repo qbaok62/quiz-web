bf1f55d11dd06eaae3d71abefe95876f6f9844ff
qbaok62 <98417594+qbaok62@users.noreply.github.com>
2022-04-01 09:41:10 +0700

    Add files via upload

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,19 +6,3 @@ export default axios.create({
     "Content-Type": "application/json",
   },
 });
-
-export const instance = axios.create({
-  baseURL: "https://fwa-ec-quiz.herokuapp.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-instance.interceptors.response.use(
-  (res) => {
-    return res.data.access;
-  },
-  async (err) => {
-    return Promise.reject(err);
-  }
-);
